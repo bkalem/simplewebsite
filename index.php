26614fecda4b1803c33a5070722b6808d02fcd52
Bilal Kalem <bkalem@users.noreply.github.com>
2024-08-13 04:22:47 +0100

    Update index.php
    availability zone

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 $public_ip = false;
-$instance_id = false; // Initialiser en cas d'échec
+$instance_id = false;
+$availability_zone = false; // Initialiser en cas d'échec
 
 if ($http_code == 200 && $token !== false) {
     // Utiliser le token pour récupérer l'adresse IP publique
@@ -31,10 +32,19 @@ if ($http_code == 200 && $token !== false) {
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-aws-ec2-metadata-token: ' . $token));
     $instance_id = curl_exec($ch);
     curl_close($ch);
+
+    // Utiliser le token pour récupérer la zone de disponibilité
+    $metadata_url_az = 'http://169.254.169.254/latest/meta-data/placement/availability-zone';
+    $ch = curl_init($metadata_url_az);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-aws-ec2-metadata-token: ' . $token));
+    $availability_zone = curl_exec($ch);
+    curl_close($ch);
 } else {
     // Affichage de l'erreur si la requête du token échoue
     $public_ip = "Erreur: Impossible d'obtenir le token IMDSv2. HTTP Code: $http_code";
     $instance_id = "Erreur: Impossible d'obtenir le token IMDSv2. HTTP Code: $http_code";
+    $availability_zone = "Erreur: Impossible d'obtenir le token IMDSv2. HTTP Code: $http_code";
 }
 
 // Vérification d'erreur pour la récupération de l'adresse IP publique
@@ -48,7 +58,13 @@ if ($instance_id === false) {
     $error = error_get_last();
     $instance_id = "Erreur: Impossible de récupérer l'instance-id. " . ($error ? $error['message'] : '');
 }
-?>  
+
+// Vérification d'erreur pour la récupération de la zone de disponibilité
+if ($availability_zone === false) {
+    $error = error_get_last();
+    $availability_zone = "Erreur: Impossible de récupérer la zone de disponibilité. " . ($error ? $error['message'] : '');
+}
+?>
 
 <!DOCTYPE HTML>
 
@@ -66,6 +82,7 @@ if ($instance_id === false) {
 							<h1>An Apache-hosted webpage</h1>
               <p>This is a website, used by <i>Bilal Kalem</i> in his AWS Training and Certification courses. Welcome!</p>
               <p>Mon adresse IP publique est : <strong><?php echo $public_ip; ?></strong></p>
+              <p>Ma Availability Zone est : <strong><?php echo $availability_zone; ?></strong></p>							
               <p>Mon instance-id est : <strong><?php echo $instance_id; ?></strong></p>
 							<ul class="actions stacked">
 								<li><a href="#" class="button large wide smooth-scroll-middle">Hosted on Amazon EC2</a></li>
